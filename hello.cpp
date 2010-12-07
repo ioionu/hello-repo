e44f239991d3778e75de4141b80dc113fdcd3f73
ちくび <io@ioio.nu>
2010-12-07 20:04:25 +1100

    first commmmit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int shit = 1;
+
+int main()
+{
+//  int x = 5;
+  cout << shit;
+  shit = 3;
+  cout << shit;
+  int shit = 2;
+  cout << shit;
+  cout << ::shit;
+  return 0;
+}
+
+
+
